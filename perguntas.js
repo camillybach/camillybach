81137f27cc3312d27d9ec91828efbcba9e50a5b9
camillybach <camilly.bach.silva@escola.pr.gov.br>
2024-10-02 14:47:40 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -7,6 +7,13 @@ criaCartao(
 
 criaCartao(
     'Geografia',
+    'Qual capital do Brasil"', 
+    'Brasilia'
+    )
+
+
+criaCartao(
+    'Geografia',
     'Quantos corações tem um polvo??',
     'Ele tem 3'
 )
@@ -23,3 +30,36 @@ criaCartao(
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
+
+criaCartao(
+    'Programação',
+    'Quem era o antigo Deus Grego do Sol? "', 
+    'Apolo'
+    )
+
+
+criaCartao(
+    'Geografia',
+    'Em que ano foi criada a ONU? ',
+    '1945'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a 4ª letra do alfabeto grego?',
+    ' Delta'
+
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual planeta tem o maior número de luas? ',
+    'Saturno'
+)
+criaCartao(
+    'Programação',
+    'Qual país tem mais títulos da Copa do Mundo?"', 
+    'Brasil'
+    )
+
+
