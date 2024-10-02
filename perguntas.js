e41509f161b4c34200883bf1f2ebe15fb33844e5
camillybach <camilly.bach.silva@escola.pr.gov.br>
2024-10-02 14:52:16 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,65 +1,73 @@
-criaCartao(
+criaCartao
+    (
     'Programação',
     'Qual cidade é conhecida como "A Cidade Eterna?"', 
     'Roma'
     )
 
 
-criaCartao(
+criaCartao
+    (
     'Geografia',
     'Qual capital do Brasil"', 
     'Brasilia'
     )
 
 
-criaCartao(
+criaCartao
+    (
     'Geografia',
     'Quantos corações tem um polvo??',
     'Ele tem 3'
 )
 
-criaCartao(
+criaCartao
+    (
     'Programação',
     'Qual planeta está mais próximo do sol??',
     ' Mercúrio
 
 )
 
-criaCartao(
+criaCartao
+    (
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
 
-criaCartao(
+criaCartao
+    (
     'Programação',
     'Quem era o antigo Deus Grego do Sol? "', 
     'Apolo'
     )
 
 
-criaCartao(
+criaCartao
+    (
     'Geografia',
     'Em que ano foi criada a ONU? ',
     '1945'
 )
 
-criaCartao(
+criaCartao
+    (
     'Programação',
     'Qual é a 4ª letra do alfabeto grego?',
     ' Delta'
 
 )
 
-criaCartao(
+criaCartao
+    (
     'Lingua inglesa',
     'Qual planeta tem o maior número de luas? ',
     'Saturno'
 )
-criaCartao(
+criaCartao
+    (
     'Programação',
     'Qual país tem mais títulos da Copa do Mundo?"', 
     'Brasil'
     )
-
-
